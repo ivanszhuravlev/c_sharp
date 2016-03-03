790ab082db5e3ff6ff4fe2192aa3d68f2f36def8
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-03-03 22:14:48 +0300

    Democracy problem

---
+++ 02_democracy_problem/02_democracy_problem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+** Вам надо написать программу, которая определяет по заданному разбиению избирателей на группы 
+** минимальное количество сторонников партии, достаточное, при некотором распределении их по группам, 
+** для принятия любого решения. 
+
+*** Исходные данные
+
+** В первой строке записано нечётное число K — количество групп избирателей (1 ≤ K ≤ 101). 
+** Во второй строке через пробел записаны K нечётных чисел, которые задают количество избирателей в группах. 
+** Население острова не превосходит 9999 человек.
+
+*** Результат
+
+** Выведите минимальное количество сторонников партии, способное решить исход голосования.
+*/
+
+namespace _02_democracy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine());
+            string[] Str = Console.ReadLine().Split(' ');
+            int[] groups = new int[Str.Length];
+            for (int i = 0; i < K; i++)
+            {
+                groups[i] = int.Parse(Str[i]);
+            }
+
+            Array.Sort(groups);
+
+            int res = 0;
+
+            for (int i = 0; i < K / 2 + 1; i++)
+            {
+                res += groups[i] / 2 + 1;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ 02_democracy_problem/02_democracy_problem/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("02_democracy_problem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_democracy_problem")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b3579001-7343-4546-9206-397af3ab7488")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
