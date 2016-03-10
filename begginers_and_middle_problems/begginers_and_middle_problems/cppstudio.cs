e794f3c0cedb3af9499f51e9f52d038e87e03e1d
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-03-10 05:54:53 +0300

    cppstudio problem

---
+++ begginers_and_middle_problems/begginers_and_middle_problems/cppstudio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+*** Задание
+** Составить программу, которая будет считывать введённое пятизначное число. 
+** После чего, каждую цифру этого числа необходимо вывести в новой строке.
+*/
+
+namespace begginers_and_middle_problems
+{
+    class cppstudio
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int x = int.Parse(str),
+                i = 1;
+            double length = Math.Pow(10, (double)str.Length - 1);
+
+            while (x != 0)
+            {
+                Console.WriteLine(i + " цифра равна " + x / (int) length);
+                x %= (int) length;
+                i++;
+                length /= 10;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
